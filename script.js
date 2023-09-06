fe14306f3f36f4cbe1feb19dd1fb128e99c0fa75
maqu123 <141263646+maqu123@users.noreply.github.com>
2023-09-06 15:37:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,63 @@
-// Sticky Navigation Menu JS Code
-let nav = document.querySelector("nav");
-let scrollBtn = document.querySelector(".scroll-button a");
-console.log(scrollBtn);
-let val;
-window.onscroll = function() {
-  if(document.documentElement.scrollTop > 20){
-    nav.classList.add("sticky");
-    scrollBtn.style.display = "block";
-  }else{
-    nav.classList.remove("sticky");
-    scrollBtn.style.display = "none";
-  }
-
-}
-
-// Side NavIgation Menu JS Code
-let body = document.querySelector("body");
-let navBar = document.querySelector(".navbar");
-let menuBtn = document.querySelector(".menu-btn");
-let cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = function(){
-  navBar.classList.add("active");
-  menuBtn.style.opacity = "0";
-  menuBtn.style.pointerEvents = "none";
-  body.style.overflow = "hidden";
-  scrollBtn.style.pointerEvents = "none";
-}
-cancelBtn.onclick = function(){
-  navBar.classList.remove("active");
-  menuBtn.style.opacity = "1";
-  menuBtn.style.pointerEvents = "auto";
-  body.style.overflow = "auto";
-  scrollBtn.style.pointerEvents = "auto";
-}
-
-// Side Navigation Bar Close While We Click On Navigation Links
-let navLinks = document.querySelectorAll(".menu li a");
-for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click" , function() {
-    navBar.classList.remove("active");
-    menuBtn.style.opacity = "1";
-    menuBtn.style.pointerEvents = "auto";
-  });
-}
+// IEFE
+(() => {
+    // state variables
+    let toDoListArray = [];
+    // ui variables
+    const form = document.querySelector(".form");
+    const input = form.querySelector(".form__input");
+    const ul = document.querySelector(".toDoList");
+  
+    // event listeners
+    form.addEventListener('submit', e => {
+      // prevent default behaviour - Page reload
+      e.preventDefault();
+      // give item a unique ID
+      let itemId = String(Date.now());
+      // get/assign input value
+      let toDoItem = input.value;
+      //pass ID and item into functions
+      addItemToDOM(itemId , toDoItem);
+      addItemToArray(itemId, toDoItem);
+      // clear the input box. (this is default behaviour but we got rid of that)
+      input.value = '';
+    });
+  
+    ul.addEventListener('click', e => {
+      let id = e.target.getAttribute('data-id')
+      if (!id) return // user clicked in something else
+      //pass id through to functions
+      removeItemFromDOM(id);
+      removeItemFromArray(id);
+    });
+  
+    // functions
+    function addItemToDOM(itemId, toDoItem) {
+      // create an li
+      const li = document.createElement('li')
+      li.setAttribute("data-id", itemId);
+      // add toDoItem text to li
+      li.innerText = toDoItem
+      // add li to the DOM
+      ul.appendChild(li);
+    }
+  
+    function addItemToArray(itemId, toDoItem) {
+      // add item to array as an object with an ID so we can find and delete it later
+      toDoListArray.push({ itemId, toDoItem});
+      console.log(toDoListArray)
+    }
+  
+    function removeItemFromDOM(id) {
+      // get the list item by data ID
+      var li = document.querySelector('[data-id="' + id + '"]');
+      // remove list item
+      ul.removeChild(li);
+    }
+  
+    function removeItemFromArray(id) {
+      // create a new toDoListArray with all li's that don't match the ID
+      toDoListArray = toDoListArray.filter(item => item.itemId !== id);
+      console.log(toDoListArray);
+    }
+  
+  })();
